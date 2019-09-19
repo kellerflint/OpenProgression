8fd1d922749884e40dd4e61f44a9a3ea4a589a4e
kellerflint <kflint0068@gmail.com>
2019-09-18 20:04:21 -0700

    restructured filesystem

--- app/private/shared/default_header.php
+++ app/private/shared/default_header.php
@@ -1,18 +1,16 @@
-<?php
-
-?>
-
 <!doctype html>
 <html>
 
     <head>
         <meta charset="UTF-8">
         <title>MyMakerSite</title>
+        <link rel="stylesheet" href="<?php echo url_for('public_html/style/default.css'); ?>">
     </head>
 
     <body>
+    
         <nav>
             <ul>
-               
+                <li><a href="<?php echo url_for('public_html/'); ?>">Login</a></li>
             </ul>
         </nav>
