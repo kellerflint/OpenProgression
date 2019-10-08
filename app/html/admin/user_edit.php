e13e0cf87c7f501d6766efb39f918ce117c15278
kellerflint <kflint0068@gmail.com>
2019-10-08 14:53:00 -0700

    housekeeping

--- app/html/admin/user_edit.php
+++ app/html/admin/user_edit.php
@@ -3,14 +3,16 @@ require_permission(ADM);
 
 $style = "/style/user_edit.css";
 
-if (request_is_post()) {
+if (request_is_post() && $_POST['value'] == "add") {
     $user_nickname = $_POST['nickname'];
     add_user($user_nickname, $_SESSION['session_id']);
-}
+} else if (request_is_post() && $_POST['value'] == "edit") { }
 
 
 include_once '../../private/shared/default_header.php'; ?>
 
+
+<!--
 <div>
     <h2>Add User</h2>
     <form action="user_edit.php" method="POST">
@@ -19,9 +21,6 @@ include_once '../../private/shared/default_header.php'; ?>
         <button name="submit" value="add">Add User</button>
     </form>
 </div>
-
-<div>
-    <h2>Edit User</h2>
-</div>
+-->
 
 <?php include_once '../../private/shared/default_footer.php'; ?>
