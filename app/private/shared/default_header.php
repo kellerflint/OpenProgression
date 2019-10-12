68cfdc43ce1a3d23e3c0b831a65151b1d7699e51
kellerflint <kflint0068@gmail.com>
2019-10-11 23:09:13 -0700

    changed name of site

--- app/private/shared/default_header.php
+++ app/private/shared/default_header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>MyMakerSite</title>
+    <title>OpenProgression</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo url_for('style/default.css'); ?>">
     <link rel="stylesheet" href="<?php echo $style; ?>">
@@ -12,7 +12,7 @@
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">MyMakerSite</a>
+        <a class="navbar-brand" href="#">OpenProgression</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
