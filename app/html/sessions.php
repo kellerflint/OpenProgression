2e84e4f38341794402920ab69729b971da7a7e5e
kellerflint <kflint0068@gmail.com>
2019-10-12 18:42:55 -0700

    updated sessions to use bootstrap

--- app/html/sessions.php
+++ app/html/sessions.php
@@ -10,20 +10,20 @@ if (request_is_post()) {
 include_once '../private/shared/default_header.php'; ?>
 
 <div class="content">
-    <h2>My Sessions</h2>
-    <?php 
+    <h2 class="heading text-center">My Sessions</h2>
+    <?php
     $session_set = find_user_sessions($_SESSION['user_id']);
-    while($session = mysqli_fetch_assoc($session_set)) {
-    ?>
-    <form action="sessions.php" method="POST">
-        <input class="hidden" name="session_id" type="text" value="<?php echo $session['session_id'] ?>">
-        <p><?php echo $session['session_name']; ?></p>
-        <button name="submit" value="connect">Connect</button>
-        <br>
-        <div class="descript hidden">
-            <p><?php echo $session["session_description"]; ?></p>
-        </div>
-    </form>
+    while ($session = mysqli_fetch_assoc($session_set)) {
+        ?>
+        <form action="sessions.php" class="session border bg-light" method="POST">
+            <input class="hidden" name="session_id" type="text" value="<?php echo $session['session_id'] ?>">
+            <p><?php echo $session['session_name']; ?></p>
+            <button type="submit" name="submit" class="btn btn-primary" value="connect">Connect</button>
+            <br>
+            <div class="descript hidden">
+                <p><?php echo $session["session_description"]; ?></p>
+            </div>
+        </form>
     <?php } ?>
 
 </div>
